a1388701189f11a48cc2be43ece409bbd5354fd8
Eva <>
2021-03-25 08:53:34 -0600

    Completed book test

--- src/book.js
+++ src/book.js
@@ -1,12 +1,46 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+
 }
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return character;
+}
+function saveReview(newReview, review) {
+  if (review.includes(newReview)) {
+      return review;
+    } else {
+        return review.push(newReview)
+    }
+}
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+function writeBook(book, bookCharacter, genre) {
+  var book = {
+      title: book,
+      mainCharacter: bookCharacter,
+      pageCount: calculatePageCount(book),
+      genre: genre
+    }
+    return book;
+}
+function editBook(book) {
+    book.pageCount = book.pageCount * .75;
+
+}
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
