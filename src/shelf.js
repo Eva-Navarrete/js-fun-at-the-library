33260c56e57abe4a56b31f8d7abb117517bd0eb0
Eva <>
2021-03-25 20:47:50 -0600

    Completed shelf test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,39 @@
+function shelfBook(book, shelf) {
+ if (shelf.length < 3) {
+    return shelf.unshift(book);
+  } else {
+    return;
+  }
+}
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      shelf.splice(i,1)
+    }
+  }
+  return;
+}
+function listTitles(shelf) {
+  var list = [];
+    for (var i = 0; i < shelf.length; i++) {
+      list.push(shelf[i].title);
+    }
+    return list.join(", ")
+}
+function searchShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title)  {
+      return true;
+    }
+  }
+     return false;
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
