0d2acf7b949fb75f11ff200a3419eb36d460cef0
Eva <>
2021-03-26 17:05:16 -0600

    Completed library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,35 @@
-
+function createLibrary(library) {
+  var newLibrary = {
+      name: library,
+      shelves: {
+        fantasy: [],
+        fiction: [],
+        nonFiction: []
+      },
+    };
+    return newLibrary;
+}
+function addBook(newLibrary, book) {
+  if (book.genre === "fantasy") {
+    return newLibrary.shelves.fantasy.push(book);
+  } else if (book.genre === "fiction") {
+    return newLibrary.shelves.fiction.push(book);
+  } else {
+    return newLibrary.shelves.nonFiction.push(book);
+  }
+}
+function checkoutBook(denverLibrary, title, genre) {
+  for (i = 0; i < denverLibrary.shelves[genre].length; i++) {
+      if (title === denverLibrary.shelves[genre][i].title) {
+        denverLibrary.shelves[genre].splice(i, 1)
+        return `You have now checked out ${title} from the ${denverLibrary.name}`
+      }
+    }
+    return `Sorry, there are currently no copies of ${title} available at the ${denverLibrary.name}`
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook
+  createLibrary,
+  addBook,
+  checkoutBook
 };
--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,33 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+
   }
+  greetPatron(name, morning) {
+    if (morning === true) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+
+    }
+  };
+  findBook(bookTitle){
+    var newBook = this.library.shelves['fantasy'][0]['title'];
+    var saying;
+    if (bookTitle === newBook) {
+      saying = `Yes, we have ${bookTitle}`
+      this.library.shelves['fantasy'].pop()
+    } else {
+      saying = `Sorry, we do not have ${bookTitle}`
+    }
+    return saying
+  };
+calculateLateFee(daysLate) {
+var lateFee = daysLate * .25;
+return Math.ceil(lateFee);
 }
+}
+
 
 module.exports = Librarian;
